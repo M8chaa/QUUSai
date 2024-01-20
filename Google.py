fa96831e5687f09235a8a4c085a716f5f07583a9
M8chaa <m8chaa@gmail.com>
2024-01-20 15:29:40 +0900

    sheet drive swap to json

--- Google.py
+++ Google.py
@@ -1,20 +1,15 @@
 import pickle
 import os
-from os import path
 from google_auth_oauthlib.flow import Flow, InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
 from google.auth.transport.requests import Request
-import json
+from google.oauth2.credentials import Credentials
+from googleapiclient.errors import HttpError
 
-
-# def Create_Service(client_secret_file, api_name, api_version, *scopes):
-def Create_Service(client_secret_data, api_name, api_version, *scopes):
-    # print(client_secret_file, api_name, api_version, scopes, sep='-')
-    print(client_secret_data, api_name, api_version, scopes, sep='-')
-    # CLIENT_SECRET_FILE = client_secret_file
-    if isinstance(client_secret_data, str):
-        client_secret_data = json.loads(client_secret_data)
+def Create_Service(client_secret_file, api_name, api_version, *scopes):
+    print(client_secret_file, api_name, api_version, scopes, sep='-')
+    CLIENT_SECRET_FILE = client_secret_file
     API_SERVICE_NAME = api_name
     API_VERSION = api_version
     SCOPES = [scope for scope in scopes[0]]
@@ -22,31 +17,32 @@ def Create_Service(client_secret_data, api_name, api_version, *scopes):
 
     cred = None
 
-    pickle_file = f'token_{API_SERVICE_NAME}_{API_VERSION}.pickle'
-    # print(pickle_file)
-
-    if path.exists(pickle_file):
-        with open(pickle_file, 'rb') as token:
-            cred = pickle.load(token)
-
+    pickle_file = f'token_{API_VERSION}.pickle'
+    print(pickle_file)
+    # if os.path.exists(pickle_file):
+    #     with open(pickle_file, 'rb') as token:
+    #         cred = pickle.load(token)
+    
+    if os.path.exists("token.json"):
+        cred = Credentials.from_authorized_user_file("token.json", SCOPES)
+    
     if not cred or not cred.valid:
         if cred and cred.expired and cred.refresh_token:
             cred.refresh(Request())
+            print("token refreshed")
         else:
-            # flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
-            flow = InstalledAppFlow.from_client_config(client_secret_data, SCOPES)
-            auth_url, _ = flow.authorization_url()
+            flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
             cred = flow.run_local_server()
-            #cred = flow.redirect_uri
+            print("token recreated")
+            # cred1 = flow.redirect_uri()
 
-
-
-        with open(pickle_file, 'wb') as token:
-            pickle.dump(cred, token)
-
+        # with open(pickle_file, 'wb') as token:
+            # pickle.dump(cred, token)
+        with open("token.json", "w") as token:
+            token.write(cred.to_json())
     try:
         service = build(API_SERVICE_NAME, API_VERSION, credentials=cred)
-        print(API_SERVICE_NAME, 'service created successfully')
+        print(API_SERVICE_NAME, 'Cred valid. Service created successfully')
         return service
     except Exception as e:
         print('Unable to connect.')
@@ -56,4 +52,3 @@ def Create_Service(client_secret_data, api_name, api_version, *scopes):
 # def convert_to_RFC_datetime(year=1900, month=1, day=1, hour=0, minute=0):
 #     dt = datetime.datetime(year, month, day, hour, minute, 0).isoformat() + 'Z'
 #     return dt
-
