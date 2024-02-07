79bc82ab56f438ceff2d85e64c6b62e90de612fa
M8chaa <m8chaa@gmail.com>
2024-02-07 14:52:55 +0900

    thread number adjusted

--- pages/04_CrawlingAI_for_Moyo.py
+++ pages/04_CrawlingAI_for_Moyo.py
@@ -656,7 +656,7 @@ def moyocrawling_Just_Moyos(sheet_id, sheetUrl):
 
      # Start data fetching threads
     fetch_threads = []
-    for _ in range(5):
+    for _ in range(4):
         driver = setup_driver()  # Each thread gets its own driver instance
         t = threading.Thread(target=fetch_data_Just_Moyos, args=(driver, url_fetch_queue, data_queue))
         t.start()
