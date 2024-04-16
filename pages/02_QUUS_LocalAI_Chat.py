0ad20f0d92cd8397bf9ffbf9c7df18a450cbc9c2
M8chaa <m8chaa@gmail.com>
2024-04-16 16:04:08 +0900

    chat page added

---
+++ pages/02_QUUS_LocalAI_Chat.py
@@ -0,0 +1,70 @@
+import streamlit as st
+from langchain.chat_models import ChatOllama
+from langchain.callbacks.base import BaseCallbackHandler
+from langserve import RemoteRunnable
+from langchain_core.runnables.schema import StreamEvent
+
+st.set_page_config(
+    page_title="쿠스AI",
+    page_icon="🔒",
+)
+
+openaikey = st.secrets["OPENAI_API_KEY"]
+#log openaikey
+st.write(openaikey)
+ip = st.secrets["Langserve_endpoint"]
+LANGSERVE_ENDPOINT = f"http://{ip}/chat/c/N4XyA"
+
+class ChatCallbackHandler(BaseCallbackHandler):
+    message = ""
+
+    def on_llm_start(self, *args, **kwargs):
+        self.message_box = st.empty()
+
+    def on_llm_end(self, *args, **kwargs):
+        save_message(self.message, "ai")
+
+    def on_llm_new_token(self, token, *args, **kwargs):
+        self.message += token
+        self.message_box.markdown(self.message)
+
+llm = RemoteRunnable(LANGSERVE_ENDPOINT)
+
+def save_message(message, role):
+    st.session_state["messages"].append({"message": message, "role": role})
+
+def send_message(message, role, save=True):
+    with st.chat_message(role):
+        st.markdown(message)
+    if save:
+        save_message(message, role)
+
+def paint_history():
+    for message in st.session_state["messages"]:
+        send_message(
+            message["message"],
+            message["role"],
+            save=False,
+        )
+
+st.title("쿠스AI")
+
+st.markdown(
+    """
+Welcome!
+            
+Use this chatbot to ask questions to an AI!
+
+"""
+)
+
+send_message("I'm ready! Ask away!", "ai", save=False)
+paint_history()
+message = st.chat_input("Ask anything...")
+if message:
+    send_message(message, "human")
+    with st.chat_message("ai"):
+        llm.invoke(message)
+
+else:
+    st.session_state["messages"] = []
