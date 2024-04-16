2a593d40056b825f687badaa1e2b7e0263545e82
M8chaa <m8chaa@gmail.com>
2024-04-16 11:42:05 +0900

    dependencies

--- pages/02_QUUS_LocalAI.py
+++ pages/02_QUUS_LocalAI.py
@@ -16,6 +16,13 @@ st.set_page_config(
     page_title="쿠스AI",
     page_icon="🔒",
 )
+from pydantic import BaseModel
+
+class MyModel(BaseModel):
+    my_field: MyCustomType
+
+    class Config:
+        arbitrary_types_allowed = True
 
 openaikey = st.secrets["OPENAI_API_KEY"]
 ip = st.secrets["Langserve_endpoint"]
