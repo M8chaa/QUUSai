4e05db52b561507f27a791f0c9d323efc0f1d834
MinsooCha <m8chaa@gmail.com>
2024-07-25 12:42:06 +0900

    Nokcci ai added

--- pages/07_Nokcci_AI.py
+++ pages/07_Nokcci_AI.py
@@ -6,26 +6,40 @@ st.set_page_config(
     layout="wide"
 )
 
+# Add custom CSS to adjust spacing and iframe size
+st.markdown(
+    """
+    <style>
+    .main .block-container {
+        padding-top: 0rem;
+        padding-left: 0rem;
+        padding-right: 0rem;
+        padding-bottom: 0rem;
+    }
+    .full-height-iframe {
+        height: calc(100vh - 4rem); /* Adjust height to fit viewport minus header */
+        width: 100%;
+        border: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 st.markdown(
     """
     # NokcciAI
-            
-    ###Extract any speech from audio files.
-            
+
+    ### Extract any speech from audio files.
+    
     Upload your audio files then it will transcribe them for you.
-"""
+    """,
+    unsafe_allow_html=True
 )
 
 # Embed the website in the main screen
 st.markdown(
     """
-    <style>
-    .full-height-iframe {
-        height: 100vh;
-        width: 100%;
-        border: none;
-    }
-    </style>
     <iframe src="https://www.nokcci.com/" class="full-height-iframe"></iframe>
     """,
     unsafe_allow_html=True
